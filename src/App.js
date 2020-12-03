f9137ed419eb31ce15769a769c0a0301008e8629
Kelsey Oshiro <kelseymosh@gmail.com>
2020-12-03 16:29:15 -0700

    added sprites

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import './App.css';
-// import PokemonContainer from "./containers/PokemonContainer"
+import PokemonContainer from "./containers/PokemonContainer";
 
 class App extends Component {
   
@@ -11,18 +11,18 @@ class App extends Component {
   componentDidMount(){
     let fetches = [];
     let fetchPokemon = () => {
-      for (let i = 1; i <= 50; i++) {
+      for (let i = 1; i <= 300; i++) {
         fetches.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`))      
       }
     }
     fetchPokemon()
-    console.log("Promise.all(fetches)", Promise.all(fetches))
+    // console.log("Promise.all(fetches)", Promise.all(fetches))
     Promise.all(fetches).then(responses => {
-      console.log("responses", responses) 
+      // console.log("responses", responses) 
       const parsedResponses = responses.map(response => response.json())
 
-      console.log("parsed responses", parsedResponses)
-      console.log("Promise.all(parsedResponses)", Promise.all(parsedResponses))
+      // console.log("parsed responses", parsedResponses)
+      // console.log("Promise.all(parsedResponses)", Promise.all(parsedResponses))
       Promise.all(parsedResponses).then(pokemon => this.setState({ pokemon }))
     }) 
   }
@@ -30,7 +30,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        {/* <PokemonContainer pokemon={this.state.pokemon}/> */}
+          <PokemonContainer pokemon={this.state.pokemon} />
       </div>
     );
     }
