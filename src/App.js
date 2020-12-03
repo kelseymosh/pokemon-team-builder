16783d4f4364bab034a8678584eb36735e328d5e
Kelsey Oshiro <kelseymosh@gmail.com>
2020-12-03 15:54:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+// import PokemonContainer from "./containers/PokemonContainer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    pokemon: []
+  }
+
+  componentDidMount(){
+    let fetches = [];
+    let fetchPokemon = () => {
+      for (let i = 1; i <= 50; i++) {
+        fetches.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`))      
+      }
+    }
+    fetchPokemon()
+    console.log("Promise.all(fetches)", Promise.all(fetches))
+    Promise.all(fetches).then(responses => {
+      console.log("responses", responses) 
+      const parsedResponses = responses.map(response => response.json())
+
+      console.log("parsed responses", parsedResponses)
+      console.log("Promise.all(parsedResponses)", Promise.all(parsedResponses))
+      Promise.all(parsedResponses).then(pokemon => this.setState({ pokemon }))
+    }) 
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {/* <PokemonContainer pokemon={this.state.pokemon}/> */}
+      </div>
+    );
+    }
 }
 
 export default App;
