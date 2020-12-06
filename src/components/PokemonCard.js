f54738f383c962f22e0b0cb13c9e30534abef710
Kelsey Oshiro <kelseymosh@gmail.com>
2020-12-06 11:26:24 -0700

    conditional coloring sprites

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -11,7 +11,58 @@ export default function PokemonCard({pokemon, addToTeam}) {
 
     function styleCircleColor() {
         if ( pokemon.types[0].type.name === "grass" ){
-            return "lightgreen"
+            return "#8da780"
+        }
+        if ( pokemon.types[0].type.name === "bug" ){
+            return "rgb(193,205,50)"
+        }
+        if ( pokemon.types[0].type.name === "dark" ){
+            return "#69808b"
+        }
+        if ( pokemon.types[0].type.name === "dragon" ){
+            return "#6f83d7"
+        }
+        if ( pokemon.types[0].type.name === "electric" ){
+            return "#ffdc76"
+        }
+        if ( pokemon.types[0].type.name === "fairy" ){
+            return "#f3ded7"
+        }
+        if ( pokemon.types[0].type.name === "fighting" ){
+            return "#e2dbcb"
+        }
+        if ( pokemon.types[0].type.name === "fire" ){
+            return "#ce5b0e"
+        }
+        if ( pokemon.types[0].type.name === "flying" ){
+            return "#c4d4e3"
+        }
+        if ( pokemon.types[0].type.name === "ghost" ){
+            return "#c1c0da"
+        } 
+        if ( pokemon.types[0].type.name === "ground" ){
+            return "#caab8f"
+        } 
+        if ( pokemon.types[0].type.name === "ice" ){
+            return "#c4dce3"
+        } 
+        if ( pokemon.types[0].type.name === "normal" ){
+            return "#f8f9fc"
+        } 
+        if ( pokemon.types[0].type.name === "poison" ){
+            return "#d3a4c9"
+        } 
+        if ( pokemon.types[0].type.name === "psychic" ){
+            return "#fae6fa"
+        } 
+        if ( pokemon.types[0].type.name === "rock" ){
+            return "#A28D5B"
+        } 
+        if ( pokemon.types[0].type.name === "steel" ){
+            return "#d2d1d6"
+        } 
+        if ( pokemon.types[0].type.name === "water" ){
+            return "#9cd3db"
         } else {
         }
     }
--- src/components/TeamPokemonCard.js
+++ src/components/TeamPokemonCard.js
@@ -6,9 +6,67 @@ export default function TeamPokemonCard({pokemon, removeFromTeam}) {
         removeFromTeam(pokemon)
     }
 
+    function styleCircleColor() {
+        if ( pokemon.types[0].type.name === "grass" ){
+            return "#8da780"
+        }
+        if ( pokemon.types[0].type.name === "bug" ){
+            return "rgb(193,205,50)"
+        }
+        if ( pokemon.types[0].type.name === "dark" ){
+            return "#69808b"
+        }
+        if ( pokemon.types[0].type.name === "dragon" ){
+            return "#6f83d7"
+        }
+        if ( pokemon.types[0].type.name === "electric" ){
+            return "#ffdc76"
+        }
+        if ( pokemon.types[0].type.name === "fairy" ){
+            return "#f3ded7"
+        }
+        if ( pokemon.types[0].type.name === "fighting" ){
+            return "#e2dbcb"
+        }
+        if ( pokemon.types[0].type.name === "fire" ){
+            return "#ce5b0e"
+        }
+        if ( pokemon.types[0].type.name === "flying" ){
+            return "#c4d4e3"
+        }
+        if ( pokemon.types[0].type.name === "ghost" ){
+            return "#c1c0da"
+        } 
+        if ( pokemon.types[0].type.name === "ground" ){
+            return "#caab8f"
+        } 
+        if ( pokemon.types[0].type.name === "ice" ){
+            return "#c4dce3"
+        } 
+        if ( pokemon.types[0].type.name === "normal" ){
+            return "#f8f9fc"
+        } 
+        if ( pokemon.types[0].type.name === "poison" ){
+            return "#d3a4c9"
+        } 
+        if ( pokemon.types[0].type.name === "psychic" ){
+            return "#fae6fa"
+        } 
+        if ( pokemon.types[0].type.name === "rock" ){
+            return "#A28D5B"
+        } 
+        if ( pokemon.types[0].type.name === "steel" ){
+            return "#d2d1d6"
+        } 
+        if ( pokemon.types[0].type.name === "water" ){
+            return "#9cd3db"
+        } else {
+        }
+    }
+
     return (
         <div className="team-pokemon-card">
-            <div className="team-circle">
+            <div className="team-circle" style={ {background:`${styleCircleColor()}`} }>
                 <img className="pokemon-team-art" 
                     src={pokemon.sprites.other.dream_world.front_default} 
                     alt="pokemon sprite"
@@ -16,9 +74,9 @@ export default function TeamPokemonCard({pokemon, removeFromTeam}) {
                     onClick={ handleClick }
                 />
             </div>
-                <div className="name-and-type">
+                <div className="name-and-type" style={ {background:`${styleCircleColor()}`} }>
                     {pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}
-                    {/* {pokemon.types[0].type.name} */}
+                    <img />
                 </div>
         </div>
     )
